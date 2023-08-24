e5291e82ff3e5f7b3e172a2189775b4d1d84d685
Vansh Kapoor <vanshk@Vanshs-MacBook-Pro.local>
2023-08-24 16:43:46 +0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const { Telegraf } = require('telegraf')
+require('dotenv').config()
+
+const bot = new Telegraf(process.env.TELEGRAM_API_KEY)
+
+bot.start((ctx) => {
+    ctx.reply('Welcome');
+    console.log(ctx.from);
+    console.log(ctx.chat);
+})
+
+// bot.telegram.sendMessage(914419568, "hey")
+
+bot.launch()
